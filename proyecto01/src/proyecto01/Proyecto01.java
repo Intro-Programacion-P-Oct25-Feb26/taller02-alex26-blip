44e2f2d8d0f6fe4b8431b7a878bb432de12ce20f
alex bravo <fabriciobravo373@gmail.com>
2025-10-16 12:01:52 -0500

    segundo commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -14,7 +14,11 @@ public class Proyecto01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Ejemplo de Java\n" +
+"Manejado desde GitHub\n" +
+"Versionado a través de Git\n" +
+"Creado por <ubicar su usuario de github>");
+        
     }
     
 }
